ab2ec6f9e1cf37cf81ae1833050aa323c1e8febe
Iluvatar <woquendo@gmail.com>
2022-04-18 09:14:27 -0500

    Added mods to example

--- 2022-04-06-DebuggingIntro/debug-v1.cpp
+++ 2022-04-06-DebuggingIntro/debug-v1.cpp
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-  //double x[10], y[5]; // imprime raro
-  double y[5], x[10]; // imprime 1
+  double x[10], y[5]; // imprime raro
+  //double y[5], x[10]; // imprime 1
 
   for(int ii = 0; ii < 5; ++ii) {
     y[ii] = ii +1;
