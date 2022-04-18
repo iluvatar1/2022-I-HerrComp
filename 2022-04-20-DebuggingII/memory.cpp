5eb2a9dc9b3a43426e491d319df181a5541b5c9e
Iluvatar <woquendo@gmail.com>
2022-04-18 09:01:45 -0500

    Added some debugging examepls for ddd, emacs and valgrind

---
+++ 2022-04-20-DebuggingII/memory.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+
+void getmem(double *ptr, int n);
+
+int main(int argc, char **argv)
+{
+  // declare variables
+  const int N = 10000;
+  double * array;
+  for (int ii = 0 ; ii < 20; ++ii) {
+    getmem(array, N);
+  }
+  // free memory
+  // delete [] array;
+
+  return EXIT_SUCCESS;
+}
+
+void getmem(double *ptr, int n)
+{
+  ptr = new double [n];
+  data[0] = data[1]*2;
+}
