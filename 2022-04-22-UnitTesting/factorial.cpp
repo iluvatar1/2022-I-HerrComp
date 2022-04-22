797712157f2d50e08ea4c7763ce13780dd841065
Iluvatar <woquendo@gmail.com>
2022-04-22 15:00:21 -0500

    Fixed makefile including spack

--- 2022-04-22-UnitTesting/factorial.cpp
+++ 2022-04-22-UnitTesting/factorial.cpp
@@ -1,6 +1,5 @@
 #include "factorial.h"
 
-int factorial(int number)
-{
-return number <= 1 ? number : factorial(number-1)*number;
+int factorial(int number) {
+  return number <= 1 ? number : factorial(number - 1) * number;
 }
