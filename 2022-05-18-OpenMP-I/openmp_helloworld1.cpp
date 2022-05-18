1e34e4b3c8aac73039d56bc287684b4df51acca1
Iluvatar <woquendo@gmail.com>
2022-05-18 11:18:24 -0500

    Added first openmp example

---
+++ 2022-05-18-OpenMP-I/openmp_helloworld1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include "omp.h"
+
+int main(void)
+{
+    double x = 9.0;
+//#pragma omp parallel num_threads(4)
+#pragma omp parallel
+    {// se generan los threads
+        std::printf("Hello, world.\n");
+        std::printf("Hello, world2.\n");
+    } // mueren los threads
+
+    return 0;
+}
