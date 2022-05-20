41afac57647ee63626959d331f733abb94fa80bc
Iluvatar <woquendo@gmail.com>
2022-05-20 12:39:36 -0500

    New example

--- 2022-05-20-OpenMp-II/openmp_parallel_for_manual2.cpp
+++ 2022-05-20-OpenMp-II/openmp_parallel_for_manual2.cpp
@@ -3,22 +3,23 @@
 #include <cmath>
 #include <vector>
 #include <cstdlib>
+#include <numeric>
 
 void fill(std::vector<double> & array);
+double suma(const std::vector<double> & array);
 
 int main(int argc, char *argv[])
 {
   const int N = std::atoi(argv[1]);
   std::vector<double> data(N);
-  std::cout << data.size() << "\n";
 
   // llenar el arreglo
   fill(data);
-  std::cout << data[N/2] << "\n";
+  //std::cout << data[0] << "\n";
 
   // calcular la suma y el promedio
-  //double total = suma(data);
-  //std::cout << total/data.size() << "\n";
+  double total = suma(data);
+  std::cout << total/data.size() << "\n";
 
   return 0;
 }
@@ -38,4 +39,28 @@ void fill(std::vector<double> & array)
         std::cos(std::pow(ii*ii, 0.3));
     }
   }
+}
+
+double suma(const std::vector<double> & array)
+{
+  int N = array.size();
+  std::vector<double> sumaparcial;
+#pragma omp parallel
+  {
+    int thid = omp_get_thread_num();
+    int nth = omp_get_num_threads();
+    if (0 == thid) {
+      sumaparcial.resize(nth);
+    }
+#pragma omp barrier
+    double localsum = 0.0;
+    int Nlocal = N/nth;
+    int iimin = thid*Nlocal;
+    int iimax = iimin + Nlocal;
+    for(int ii = iimin; ii < iimax; ii++) {
+      localsum += array[ii];
+    }
+    sumaparcial[thid] = localsum;
+  }
+  return std::accumulate(sumaparcial.begin(), sumaparcial.end(), 0.0);
 }
